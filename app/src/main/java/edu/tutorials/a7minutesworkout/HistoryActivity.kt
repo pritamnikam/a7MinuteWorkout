8ff54e01824d066d2d46fb43fcf113427aeee203
Pritam Nikam <pritam.nikam@gmail.com>
2021-01-27 10:03:53 +0530

    Added BMI and workout history.

---
+++ app/src/main/java/edu/tutorials/a7minutesworkout/HistoryActivity.kt
@@ -0,0 +1,61 @@
+package edu.tutorials.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_b_m_i.*
+import kotlinx.android.synthetic.main.activity_history.*
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        setSupportActionBar(toolbar_history_activity)
+
+        val actionbar = supportActionBar
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true)
+
+            actionbar.title = "History"
+        }
+
+        toolbar_history_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        getAllCompletedDays()
+    }
+
+    private fun getAllCompletedDays() {
+        val dbHandler = SqliteOpenHelper(this, null)
+        val dateList = dbHandler.getAllCompletedDatesList()
+
+
+        // TODO(Step 3 : Now here the dates which were printed in log.
+        //  We will pass that list to the adapter class which we have created and bind it to the recycler view.)
+        // START
+        if (dateList.size > 0) {
+            // Here if the List size is greater then 0 we will display the item in the recycler view or else we will show the text view that no data is available.
+            tvHistory.visibility = View.VISIBLE
+            rvHistory.visibility = View.VISIBLE
+            tvNoDataAvailable.visibility = View.GONE
+
+            // Creates a vertical Layout Manager
+            rvHistory.layoutManager = LinearLayoutManager(this)
+
+            // History adapter is initialized and the list is passed in the param.
+            val historyAdapter = HistoryAdapter(this, dateList)
+
+            // Access the RecyclerView Adapter and load the data into it
+            rvHistory.adapter = historyAdapter
+        } else {
+            tvHistory.visibility = View.GONE
+            rvHistory.visibility = View.GONE
+            tvNoDataAvailable.visibility = View.VISIBLE
+        }
+        // END
+    }
+}
